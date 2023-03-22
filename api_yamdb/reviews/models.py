4abc679253b43652de001bd5b9b676cd7160518e
Alexey Potanin <avpotanin@gmail.com>
2023-03-22 15:42:26 +0300

    reviews folder added, models review and comments created

---
+++ api_yamdb/reviews/models.py
@@ -0,0 +1,83 @@
+from django.core.validators import (
+    MaxValueValidator,
+    MinValueValidator,
+)
+from django.db import models
+
+
+class Review(models.Model):
+    """Отзывы."""
+
+    text = models.TextField(
+        verbose_name='Текст',
+    )
+    title = models.ForeignKey(
+        Title,
+        verbose_name='Произведение',
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
+    author = models.ForeignKey(
+        User,
+        verbose_name='Автор',
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
+    score = models.PositiveIntegerField(
+        verbose_name='Рейтинг',
+        validators=[
+            MinValueValidator(1, 'Минимальная оценка 1'),
+            MaxValueValidator(10, 'Максимальная оценка 10')
+        ]
+    )
+    pub_date = models.DateTimeField(
+        verbose_name='Дата публикации',
+        auto_now_add=True,
+        db_index=True
+    )
+
+    class Meta:
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
+        ordering = ['pub_date']
+        constraints = [
+            models.UniqueConstraint(
+                fields=['title', 'author'],
+                name='unique_author_title'
+            ),
+        ]
+
+    def __str__(self):
+        return self.text[:30]
+
+
+class Comment(models.Model):
+    """Комментарии."""
+    text = models.TextField(
+        verbose_name='Текст',
+    )
+    review = models.ForeignKey(
+        Review,
+        verbose_name='Отзыв',
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    author = models.ForeignKey(
+        User,
+        verbose_name='Автор',
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    created = models.DateTimeField(
+        verbose_name='Дата публикации',
+        auto_now_add=True,
+        db_index=True
+    )
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+        ordering = ['created']
+
+    def __str__(self):
+        return self.text[:30]
