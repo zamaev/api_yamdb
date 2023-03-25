1bdcabf0fd2182abc922874746ca6c113f4fcbf8
Alexey Potanin <avpotanin@gmail.com>
2023-03-25 22:42:47 +0300

    api_urls modified for reviews_comments

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,9 +1,15 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
+from .views import (CommentViewSet, ReviewViewSet,)
+
 app_name = 'api'
 
 router = DefaultRouter()
+router.register(r'titles/(?P<title_id>\d+)/reviews',
+                ReviewViewSet, basename='reviews')
+router.register(r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)'
+                r'/comments', CommentViewSet, basename='comments')
 
 urlpatterns = [
     path('v1/', include(router.urls)),
