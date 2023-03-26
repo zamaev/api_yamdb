3a0dbbde5305299fd81f512444941a8a984140c8
Aydrus Zamaev <aydrus.zamaev@gmail.com>
2023-03-26 20:16:56 +0300

    feat: more permissions

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -8,9 +8,26 @@ class isOwner(permissions.BasePermission):
 
 class isModerator(permissions.BasePermission):
     def has_permission(self, request, view):
-        return request.user.role == 'A'
+        return request.user.role == 'M'
 
 
 class isAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
         return request.user.role == 'A'
+
+
+class isOwnerModeratorAdmin(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return (request.user == obj or request.user == obj.author
+                or request.user.role == 'M' or request.user.role == 'A')
+
+
+class isOwnerAdmin(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return (request.user == obj or request.user == obj.author
+                or request.user.role == 'A')
+
+
+class isModeratorAdmin(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.role == 'M' or request.user.role == 'A'
