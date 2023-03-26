956d3a50ebf3d4cea3b8f662a82ea70fa212bb5b
Marseille <marsel.rafisovich@mail.ru>
2023-03-26 17:34:54 +0300

    corrected urls.py

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from .views import CategoryViewSet, GenreViewSet, TitleViewSet
+from api.views import CategoryViewSet, GenreViewSet, TitleViewSet
 
 app_name = 'api'
 
