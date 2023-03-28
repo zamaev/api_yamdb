6dd366ea465c5ddfc4832ee6cff81834aacd5c67
Aydrus Zamaev <aydrus.zamaev@gmail.com>
2023-03-28 22:26:41 +0300

    refact: style management

--- api_yamdb/reviews/management/commands/csvfullfillment.py
+++ api_yamdb/reviews/management/commands/csvfullfillment.py
@@ -1,7 +1,7 @@
 import csv
-from django.core.management.base import BaseCommand
 
-from reviews.models import Category, Genre, Title, GenreTitle
+from django.core.management.base import BaseCommand
+from reviews.models import Category, Genre, GenreTitle, Title
 
 CSV_FILES = {
     'category.csv': Category,
